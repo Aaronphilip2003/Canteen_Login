fedd43fdb35b2918b257950d11444325484a9178
Aaronphilip2003 <aaronphilip2003@gmail.com>
2023-02-26 17:43:22 +0530

    Initial commit

--- index.js
+++ index.js
@@ -160,6 +160,52 @@ app.get("/admin1", (req, res) => {
   );
 });
 
+app.post("/admin1/delete", (req, res) => {
+  const indices = req.body.indices;
+  const query = `DELETE FROM canteen1 WHERE index IN (${indices.join(",")})`;
+  connection.query(query, (err, results, fields) => {
+    if (err) {
+      console.error("Error deleting items from canteen1 table: ", err);
+      res.sendStatus(500);
+      return;
+    }
+    console.log(
+      `Deleted items with indices ${indices.join(",")} from canteen1 table.`
+    );
+    res.sendStatus(200);
+  });
+});
+
+app.delete("/admin1/:index", (req, res) => {
+  const index = req.params.index;
+  const query = `DELETE FROM canteen1 WHERE index = ${index}`;
+  connection.query(query, (err, results, fields) => {
+    if (err) {
+      console.error("Error deleting item from canteen1 table: ", err);
+      res.sendStatus(500);
+      return;
+    }
+    console.log(`Deleted item with index ${index} from canteen1 table.`);
+    res.sendStatus(200);
+  });
+});
+
+app.post("/admin2/delete", (req, res) => {
+  const indices = req.body.indices;
+  const query = `DELETE FROM canteen2 WHERE index IN (${indices.join(",")})`;
+  connection.query(query, (err, results, fields) => {
+    if (err) {
+      console.error("Error deleting items from canteen2 table: ", err);
+      res.sendStatus(500);
+      return;
+    }
+    console.log(
+      `Deleted items with indices ${indices.join(",")} from canteen2 table.`
+    );
+    res.sendStatus(200);
+  });
+});
+
 app.listen(8080, () => {
   console.log("Server listening on port 8080");
 });
